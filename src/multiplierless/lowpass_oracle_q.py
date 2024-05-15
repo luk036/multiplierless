6e1522b3d280576e9472f3ec339dfe24eacae321
Wai-Shing Luk <luk036@gmail.com>
2024-05-15 19:25:46 +0800

    try to retry when NoEffect

--- src/multiplierless/lowpass_oracle_q.py
+++ src/multiplierless/lowpass_oracle_q.py
@@ -29,6 +29,7 @@ class LowpassOracleQ:
         self.nnz = nnz
         self.lowpass = lowpass
         self.rcsd = np.array([0])
+        self.num_retries = 0
 
     def assess_optim_q(self, r: Arr, Spsq, retry: bool):
         """[summary]
@@ -42,15 +43,23 @@ class LowpassOracleQ:
             [type]: [description]
         """
         if not retry:  # retry due to no effect in the previous cut
-            self.lowpass.retry = False
+            # self.lowpass.retry = False
             self.lowpass.spsq = Spsq
             if cut := self.lowpass.assess_feas(r):
                 return cut, r, None, True
             h = spectral_fact(r)
             hcsd = np.array([to_decimal(to_csdfixed(hi, self.nnz)) for hi in h])
             self.rcsd = inverse_spectral_fact(hcsd)
+            self.num_retries = 0  # reset to zero
+        else:
+            # self.lowpass.retry = True
+            self.num_retries += 1
 
         (gc, hc), Spsq2 = self.lowpass.assess_optim(self.rcsd, Spsq)
-        # no more alternative cuts?
         hc += gc.dot(self.rcsd - r)
-        return (gc, hc), self.rcsd, Spsq2, not retry
+        return (
+            (gc, hc),
+            self.rcsd,
+            Spsq2,
+            self.num_retries < self.lowpass.spectrum.shape[0],
+        )
---
+++ tests/test_spectral_fact.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pytest import approx
+from multiplierless.spectral_fact import spectral_fact, inverse_spectral_fact
+
+
+def test_spectral_fact():
+    h = np.array(
+        [
+            0.76006445,
+            0.54101887,
+            0.42012073,
+            0.3157191,
+            0.10665804,
+            0.04326203,
+            0.01315678,
+        ]
+    )
+    r = inverse_spectral_fact(h)
+    h2 = spectral_fact(r)
+    assert len(h) == len(h2)
+    print(h2)
+    assert h2 == approx(h)
